8b0546745b320487ffbf4c0563764c5dc9dd1ad2
Benedikt <benedikt.viebahn@bringmeister.de>
2020-05-25 21:47:26 +0200

    icons ios

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { SafeAreaView, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import TabNavigator from "./navigators/TabNavigator";
 
 declare const global: { HermesInternal: null | {} };
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView></SafeAreaView>
-    </NavigationContainer>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </>
   );
 };
 
--- navigators/TabNavigator.tsx
+++ navigators/TabNavigator.tsx
@@ -1,12 +1,43 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
+import Icon from "react-native-vector-icons/FontAwesome";
+import Saved from "../screens/Saved";
 
-const Tab = createBottomTabNavigator();
+type TabParams = {
+  Home: undefined;
+  Saved: undefined;
+};
+
+const TabIcon: React.FC<{ route: keyof TabParams; focused: boolean }> = ({
+  route,
+  focused,
+}) => {
+  const color = focused ? "#333" : "#777";
+  switch (route) {
+    case "Home":
+      return <Icon name="home" size={24} color={color} />;
+    case "Saved":
+      return <Icon name="bookmark" size={24} color={color} />;
+    default:
+      return null;
+  }
+};
+
+const Tab = createBottomTabNavigator<TabParams>();
 
 const TabNavigator: React.FC = () => (
-  <Tab.Navigator>
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused }) => (
+        <TabIcon route={route.name} focused={focused} />
+      ),
+    })}
+    tabBarOptions={{
+      showLabel: false,
+    }}>
     <Tab.Screen name="Home" component={Home} />
+    <Tab.Screen name="Saved" component={Saved} />
   </Tab.Navigator>
 );
 
